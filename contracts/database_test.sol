32a165a4cb3b0a8025773863ceb85b27f1d11698
Denis Erfurt <denis.erfurt@gmail.com>
2016-02-23 21:36:03 +0100

    added latest package getter and deploy

--- contracts/database_test.sol
+++ contracts/database_test.sol
@@ -3,25 +3,31 @@ import 'database.sol';
 
 contract DappHubDBTest is Test {
   DappHubDB db;
-  
+
   function setUp() {
-    
     db = new DappHubDB();
-    
   }
-  
+
   function testSetPkg(  ) {
     db.setPackage("omg",1,0,0,"QmbNePoKULjaTEqS1Mr3ni3ET5aa5D4cJLWVEVruQvG3K3");
   }
-  
+
   function testPkgUpdate(  ) {
     db.setPackage("omg",1,0,0,"QmbNePoKULjaTEqS1Mr3ni3ET5aa5D4cJLWVEVruQvG3K3");
     db.setPackage("omg",2,0,0,"QmbNePoKULjaTEqS1Mr3ni3ET5aa5D4cJLWVEVruQvG3K3");
   }
-  
+
   function testMultiplePackages(  ) {
     db.setPackage("omg",1,0,0,"QmbNePoKULjaTEqS1Mr3ni3ET5aa5D4cJLWVEVruQvG3K3");
     db.setPackage("omgg",2,0,0,"QmbNePoKULjaTEqS1Mr3ni3ET5aa5D4cJLWVEVruQvG3K3");
+  }
+
+  function testLatestPkgGetter() {
+    db.setPackage("omg",1,0,0,"QmbNePoKULjaTEqS1Mr3ni3ET5aa5D4cJLWVEVruQvG3K3");
+    var (a,b,c) = db.getPackageLatest("omg");
+    assertTrue(a==1);
+    assertTrue(b==0);
+    assertTrue(c==0);
   }
 
 }
