184a1d87687b33a313fc71c0a527fa366fd7b079
Denis Erfurt <denis.erfurt@gmail.com>
2016-04-16 20:29:07 +0200

    add beta namespace and allow public publish

--- contracts/initial_controller.sol
+++ contracts/initial_controller.sol
@@ -9,6 +9,7 @@ contract DappHubNameOwnerDB is DSAuth
     function setNameOwner(bytes32 name, address owner) auth() {
         set(name, bytes32(owner));
     }
+
     // Throws if was never set.
     function getNameOwner(bytes32 name) constant returns (address) {
         return address(get(name));
@@ -22,13 +23,47 @@ contract DappHubNameOwnerDB is DSAuth
     }
 }
 
+contract DappHubBetaNameOwnerDB is DSNullMap {
+  function setNameOwner(bytes32 name, address owner) {
+    var (_owner, _ok) = tryGet(name);
+    // checks if name prefix is "beta/"
+    bool isBeta = name.length > 5 &&
+      byte(name[0]) == byte(0x62) &&
+      byte(name[1]) == byte(0x65) &&
+      byte(name[2]) == byte(0x74) &&
+      byte(name[3]) == byte(0x61) &&
+      byte(name[4]) == byte(0x2f);
+    if(!_ok && isBeta) {
+      //@log a `bytes32 name` and `bytes32 bytes32(owner)`
+      //@log owner `bytes32 _owner`
+      set(name, bytes32(owner));
+    }
+  }
+  function getNameOwner(bytes32 name) constant returns(address) {
+    return address(get(name));
+  }
+  function tryGetNameOwner(bytes32 name) 
+            constant
+            returns (address owner, bool ok)
+  {
+    var (_owner, _ok) = tryGet(name);
+    return (address(_owner), ok);
+  }
+}
+
 contract DappHubSimpleController is DSAuth {
     DappHubNameOwnerDB _name_db;
+    DappHubBetaNameOwnerDB _beta_name_db;
     DappHubDB _package_db;
     function setNameDB( DappHubNameOwnerDB name_db )
              auth()
     {
         _name_db = name_db;
+    }
+    function setBetaNameDB( DappHubBetaNameOwnerDB name_db )
+             auth()
+    {
+        _beta_name_db = name_db;
     }
     function setPackageDB( DappHubDB package_db )
              auth()
--- contracts/initial_controller_test.sol
+++ contracts/initial_controller_test.sol
@@ -5,16 +5,37 @@ import 'initial_controller.sol';
 
 contract DappHubSimpleControllerTest is Test, DSAuthModesEnum {
   DappHubNameOwnerDB names;
+  DappHubBetaNameOwnerDB betanames;
   DappHubDB packages;
   DappHubSimpleController controller;
+  Tester T1; address t1;
+  Tester T2; address t2;
 
   function setUp() {
     names = new DappHubNameOwnerDB();
+    betanames = new DappHubBetaNameOwnerDB();
     packages = new DappHubDB();
     controller = new DappHubSimpleController();
     controller.setNameDB(names);
+    controller.setBetaNameDB(betanames);
     controller.setPackageDB(packages);
     packages.updateAuthority(address(controller), DSAuthModes.Owner);
+    T1 = new Tester(); t1 = address(T1);
+    T2 = new Tester(); t2 = address(T2);
+    T1._target( betanames );
+    T2._target( names );
+  }
+
+  // Should be able to update the name db by us
+  function testUpdateNameDb() {
+    var _names = new DappHubNameOwnerDB();
+    controller.setNameDB(_names);
+  }
+
+  // Should be able to update the package db by us
+  function testUpdatePackageDb() {
+    var _packages = new DappHubDB();
+    controller.setPackageDB(_packages);
   }
 
   // Should not be able to update a package not owned by us
@@ -26,6 +47,17 @@ contract DappHubSimpleControllerTest is Test, DSAuthModesEnum {
   function testAuthorizedSetPackage() {
     names.setNameOwner("foo", address(me));
     controller.setPackage("foo", 1, 0, 0, "bar");
+  }
+
+  function testErrorClaimGlobalNamespace() {
+    DappHubNameOwnerDB(t1).setNameOwner("foo", address(t1));
+    assertTrue(names.getNameOwner("foo") == t1);
+  }
+
+  // Should allow anybody to claim names under beta/ namespace
+  function testClaimBetaNamespace() {
+   DappHubBetaNameOwnerDB(t1).setNameOwner("beta/foo", address(t1));
+   // assertTrue(names.getNameOwner("beta/foo") == t1);
   }
 
   // Should not be able to transfer a package not owned by us
